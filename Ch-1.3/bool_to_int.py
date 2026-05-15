6ce8c7f9ac578c6bd45bb2de4a731e91b9c0d14f
Tirth <tirth2318@gmail.com>
2026-05-15 23:14:01 +0530

    boolean to integer and string type casting

---
+++ Ch-1.3/bool_to_int.py
@@ -0,0 +1,8 @@
+bool_val = bool(input("Enter a boolean value: "))
+
+int_val = int(bool_val)
+str_val = str(bool_val)
+
+print("This is original boolean value: ", bool_val)
+print("This is boolean to int value: ", int_val)
+print("This is boolean to str value: ", str_val)
