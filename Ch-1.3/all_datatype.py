f964ab610f8cca8f5054b7da3ac661c46232ebe4
Tirth <tirth2318@gmail.com>
2026-05-15 23:23:18 +0530

    type and id check of data types

---
+++ Ch-1.3/all_datatype.py
@@ -0,0 +1,9 @@
+integer = int(input("Enter int value: "))
+string = str(input("Enter string value "))
+Float = float(input("Enter Float value: "))
+boolean = bool(input("Enter boolean value: "))
+
+print("Value:", integer , "Type:", type(integer) , "Id:" , id(integer))
+print("Value:", string , "Type:", type(string) , "Id:" , id(string))
+print("Value:", Float , "Type:", type(Float) , "Id:" , id(Float))
+print("Value:", boolean , "Type:", type(boolean) , "Id:" , id(boolean))
