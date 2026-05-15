74dc2be63871f025aa31f97ff04b5e63293b0c8c
Tirth <tirth2318@gmail.com>
2026-05-15 23:33:00 +0530

    find largest number using elif

---
+++ Ch-3.1/largest.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+num3 = int(input("Enter 3rd number: "))
+
+if num1 > num2:
+  print("1st number", num1 ,"is Largest")
+elif num2 > num3:
+  print("2nd number", num2 ,"is Largest")
+else:
+  print("3rd number", num3 ,"is Largest")
