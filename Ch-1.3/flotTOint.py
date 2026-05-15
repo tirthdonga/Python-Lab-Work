781c9795c989bab0e245f76c5447cc5201ec4145
Tirth <tirth2318@gmail.com>
2026-05-15 23:09:28 +0530

    float to int

---
+++ Ch-1.3/flotTOint.py
@@ -0,0 +1,6 @@
+float_num = float(input("Enter a float number: "))
+
+int_num = int(float_num)
+
+print("this is float number that user input: ", float_num)
+print("this same number that user input but it is integer now: ", int_num)
