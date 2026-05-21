b6cda825c3e2e8a961699a75decd87b7661a4657
Tirth <tirth2318@gmail.com>
2026-05-21 23:13:27 +0530

    operation upper, lower, title

---
+++ Ch-3.5/string_operation.py
@@ -0,0 +1,13 @@
+string = str(input("Enter The String: "))
+
+print(f"Uppercase: {string.upper()}")
+print(f"Lowercase: {string.lower()}")
+print(f"Title: {string.title()}")
+
+'''
+Output:
+Enter The String: TirTh
+Uppercase: TIRTH
+Lowercase: tirth
+Title: Tirth
+'''
