7e76b6f1c680e10767883524e99972aa1b1ffe7f
Tirth <tirth2318@gmail.com>
2026-05-21 22:49:19 +0530

    f-string uses

---
+++ Ch-3.5/f_string.py
@@ -0,0 +1,11 @@
+item = str(input("Enter Your Item: "))
+price = float(input("Enter The Price: "))
+
+print(f"The Price Of {item} and Price is {price}")
+
+'''
+Output:
+Enter Your Item: Apple 
+Enter The Price: 5.50
+The Price Of Apple and Price is 5.5
+'''
