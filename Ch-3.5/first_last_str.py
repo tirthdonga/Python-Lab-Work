16642ef01f02cca0c1a333c1abe308cc5bd080e0
Tirth <tirth2318@gmail.com>
2026-05-21 22:43:23 +0530

    print first name and last name from user in formate

---
+++ Ch-3.5/first_last_str.py
@@ -0,0 +1,10 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print(f"Hello, {last_name}, {first_name}!")
+
+'''
+Enter your first name: Tirth
+Enter your last name: Donga
+Hello, Donga, Tirth!
+'''
