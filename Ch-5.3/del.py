d22bc8a0c176056646f3b46659706f3cc65d7bb3
Tirth <tirth2318@gmail.com>
2026-05-26 18:54:44 +0530

    Deleting specific item in list

---
+++ Ch-5.3/del.py
@@ -0,0 +1,10 @@
+number = [1, 2, 3, 4, 5, 6, 7]
+print(f"Before Del; {number}")
+del number[1]
+print(f"After using Del: {number}")
+
+'''
+Output:
+Before Del; [1, 2, 3, 4, 5, 6, 7]
+After using Del: [1, 3, 4, 5, 6, 7]
+'''
