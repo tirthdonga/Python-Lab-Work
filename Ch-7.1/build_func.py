e717efa42e0b22be6b90f7b240e62c6bb406ea7a
Tirth <tirth2318@gmail.com>
2026-05-28 18:22:31 +0530

    use build in function

---
+++ Ch-7.1/build_func.py
@@ -0,0 +1,18 @@
+a = [1, 2, 3, 5, 4]
+
+string = "Hello World!"
+
+print(f"Lenght: {len(a)}")
+print(f"Maximun: {max(a)}")
+print(f"Soring: {sorted(a)}")
+print(f"Sum: {sum(a)}")
+print(f"Type Of: {type(a)}")
+
+'''
+Output:
+Lenght: 5
+Maximun: 5
+Soring: [1, 2, 3, 4, 5]
+Sum: 15
+Type Of: <class 'list'>
+'''
